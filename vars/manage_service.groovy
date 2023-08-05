80a874b8c86a273752b17ae2278e20ae8f837e3c
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2023-08-05 13:36:48 +0200

    test

--- vars/manage_service.groovy
+++ vars/manage_service.groovy
@@ -2,21 +2,35 @@ def call() {
     node {
         stage('Check Service Status') {
             script {
-                currentBuild.displayName = "#${HOST_NAME}: ${SERVICE} - ${COMMAND}"
+                try{
+                    // Check mandatory params
+                    assert HOST_NAME != null
+                    assert HOST_IP != null
+                    assert HOST_USER != null
+                    assert HOST_PASSWORD != null
+                    assert COMMAND != null
+                    assert SERVICE != null
 
-                def remote = [:]
-                remote.name = HOST_NAME
-                remote.host = HOST_IP
-                remote.user = HOST_USER
-                remote.password = HOST_PASSWORD
-                remote.port = 22
-                remote.allowAnyHosts = true
 
-                try {
+                    // Change build name
+                    currentBuild.displayName = "${HOST_NAME}: ${SERVICE} - ${COMMAND}"
+
+                    // Remote params
+                    def remote = [:]
+                    remote.name = HOST_NAME
+                    remote.host = HOST_IP
+                    remote.user = HOST_USER
+                    remote.password = HOST_PASSWORD
+                    remote.port = 22
+                    remote.allowAnyHosts = true
+
+                    // Execute command
                     sshCommand remote: remote, command: "systemctl ${COMMAND} ${SERVICE}", sudo: false
-                    currentBuild.description = "#${HOST_NAME}: Success"
+                    currentBuild.description = "${HOST_NAME}: Success"
+
                 } catch (Exception err) {
-                    currentBuild.description = "#${HOST_NAME}: Failed"
+                    // Build failed
+                    currentBuild.description = "${HOST_NAME}: Failed"
                     currentBuild.result = 'FAILURE'
                     error ("Failed executing command -> ${err}")
                 }
