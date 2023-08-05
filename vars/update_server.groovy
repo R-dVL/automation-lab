8904b9aae6dc51600909bcaeae936ae67321acab
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2023-08-05 04:34:35 +0200

    test

--- vars/update_server.groovy
+++ vars/update_server.groovy
@@ -1,13 +1,15 @@
 def call() {
-    stage('Demo') {
-        script {
-            def remote = [:]
-            remote.host = IP
-            remote.user = USER
-            remote.password = PASSWORD
-            remote.port = '22'
-            remote.allowAnyHosts = true
-            sshCommand remote: remote, command: "pwd"
+    node {
+        stage('Demo') {
+            script {
+                def remote = [:]
+                remote.host = IP
+                remote.user = USER
+                remote.password = PASSWORD
+                remote.port = '22'
+                remote.allowAnyHosts = true
+                sshCommand remote: remote, command: "pwd"
+            }
         }
     }
 }
