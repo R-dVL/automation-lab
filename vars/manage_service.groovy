0ad52571dc50905c078d316b059d0aa617fc9fa7
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2023-08-05 14:48:31 +0200

    test

--- vars/manage_service.groovy
+++ vars/manage_service.groovy
@@ -1,18 +1,35 @@
 def call() {
     node {
-        stage('Check Service Status') {
-            script {
-                try{
-                    // Check mandatory params
-                    try {
-                        assert HOST_USER == "" : "HOST_NAME is a Mandatory parameter"
-                    } catch (AssertionError err) {
-                        throw err
-                    }
+        environment{
+            HOST_NAME
+            HOST_IP
+            HOST_USER
+            HOST_PASSWORD
+        }
 
-                    // Change build name
-                    currentBuild.displayName = "${HOST_NAME}: ${SERVICE} - ${COMMAND}"
+        try {
+            stage('Configure Host') {
+                switch(HOST) {
+                    case "Server":
+                        HOST_NAME = 'rdvl-server'
 
+                        withCredentials([
+                            usernamePassword(credentialsId: 'server-credentials',
+                                usernameVariable = HOST_USER,
+                                passwordVariable = HOST_PASSWORD)
+                        ])
+
+                        withCredentials([
+                            string(credentialsId: 'server-ip',
+                                variable = HOST_IP,
+                        ])
+                }
+                // Change build name
+                currentBuild.displayName = "${HOST_NAME}: ${SERVICE} - ${COMMAND}"
+            }
+
+            stage('Execute Command') {
+                script {
                     // Remote params
                     def remote = [:]
                     remote.name = HOST_NAME
@@ -25,14 +42,13 @@ def call() {
                     // Execute command
                     sshCommand remote: remote, command: "systemctl ${COMMAND} ${SERVICE}", sudo: false
                     currentBuild.description = "${HOST_NAME}: Success"
-
-                } catch (Exception err) {
-                    // Build failed
-                    currentBuild.description = "${HOST_NAME}: Failed"
-                    currentBuild.result = 'FAILURE'
-                    error ("Failed executing command -> ${err}")
                 }
             }
+        } catch (Exception err) {
+            // Build failed
+            currentBuild.description = "${HOST_NAME}: Failed"
+            currentBuild.result = 'FAILURE'
+            error ("Failed executing command -> ${err}")
         }
     }
 }
