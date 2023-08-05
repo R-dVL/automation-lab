1513e2ba46cbe4fe8e0f73fa22fe702ff2766f31
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2023-08-05 04:57:46 +0200

    fix

--- vars/update_server.groovy
+++ vars/update_server.groovy
@@ -9,7 +9,6 @@ def call() {
                 remote.password = HOST_PASSWORD
                 remote.port = 22
                 remote.allowAnyHosts = true
-                remote.sudo = true
                 sshCommand remote: remote, command: "apt update && apt upgrade"
             }
         }
