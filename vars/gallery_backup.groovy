f411d3de0828b74d28c8c6a42717d4014795fe9c
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2023-08-06 03:49:48 +0200

    test

---
+++ vars/gallery_backup.groovy
@@ -0,0 +1,35 @@
+def call() {
+    node {
+        try {
+            stage('Execute Command') {
+                script {
+                    withCredentials([
+                        usernamePassword(
+                            credentialsId: 'server-credentials',
+                            usernameVariable: 'user',
+                            passwordVariable: 'password')
+                    ]) {
+                        // Remote params
+                        def remote = [:]
+                        remote.name = 'rdvl-server'
+                        remote.host = '212.169.220.230'
+                        remote.user = user
+                        remote.password = password
+                        remote.port = 22
+                        remote.allowAnyHosts = true
+
+                        // Execute command
+                        sshCommand(
+                            remote: remote,
+                            command: "tar -czvf /opt/backups/gallery/test.tar.gz /DATA/Gallery")
+                    }
+                }
+            }
+
+        } catch (Exception err) {
+            // Build failed
+            currentBuild.result = 'FAILURE'
+            error ("Failed executing command -> ${err}")
+        }
+    }
+}
