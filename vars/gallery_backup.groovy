d52c6aec471f2a53ed7ee6d56ea4dc6d5d14305c
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2023-08-06 12:50:55 +0200

    test

--- vars/gallery_backup.groovy
+++ vars/gallery_backup.groovy
@@ -1,7 +1,9 @@
+import java.time.LocalDateTime
+
 def call() {
     node {
         try {
-            stage('Execute Command') {
+            stage('Create Backup') {
                 script {
                     withCredentials([
                         usernamePassword(
@@ -18,10 +20,41 @@ def call() {
                         remote.port = 22
                         remote.allowAnyHosts = true
 
+                        // Get actual date and time
+                        def dt = LocalDateTime.now()
+
                         // Execute command
                         sshCommand(
                             remote: remote,
-                            command: "tar -czvf /DATA/Backups/Gallery/test.tar.gz /DATA/Gallery")
+                            command: "tar -czvf /DATA/Backups/Gallery/${dt}.tar.gz /DATA/Gallery")
+                    }
+                }
+            }
+
+            stage('Delete Old backups') {
+                script {
+                    withCredentials([
+                        usernamePassword(
+                            credentialsId: 'server-credentials',
+                            usernameVariable: 'user',
+                            passwordVariable: 'password')
+                    ]) {
+                        // Remote params
+                        def remote = [:]
+                        remote.name = 'rdvl-server'
+                        remote.host = '212.169.220.230'
+                        remote.user = user
+                        remote.password = password
+                        remote.port = 22
+                        remote.allowAnyHosts = true
+                        def dt = 'test'
+                        // Execute command
+                        sshCommand(
+                            remote: remote,
+                            command: "'
+                                cd /DATA/Backups/Gallery
+                                find . ! -name "${dt}.tar.gz" -type f -exec rm -f {} \;
+                            '")
                     }
                 }
             }
