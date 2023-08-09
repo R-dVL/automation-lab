7aa763f91547a811ffcb8c4bf79ffcf101813fa4
R-dVL <rauldel.valle.lima@gmail.com>
2023-08-09 11:57:40 +0200

    fix

--- vars/test_pipeline.groovy
+++ vars/test_pipeline.groovy
@@ -11,9 +11,8 @@ def call() {
                     print(host)
             }
         } catch(Exception err) {
-            println(" ALERT | Something went wrong")
-            println("MESSAGE| ${err.getMessage()}")
-            error(err)
+            println("ALERT | Something went wrong")
+            error("MESSAGE | ${err.getMessage()}")
         }
     }
 }
