fb1827406de44c693d9cd0f051fedc36a51314fb
R-dVL <rauldel.valle.lima@gmail.com>
2023-08-09 11:36:01 +0200

    test

--- vars/test_pipeline.groovy
+++ vars/test_pipeline.groovy
@@ -1,13 +1,18 @@
 package io.rdvl.automationLibrary
 
 def call() {
-    node {
-        stage('Test'){
-                // Clean before build
-                cleanWs()
-                sh('git clone https://github.com/R-dVL/automation-lab.git')
-                Host host = new Host(HOST)
-                print(host)
+    node ('Linux') {
+        try {
+            stage('Test'){
+                    // Clean before build
+                    cleanWs()
+                    sh('git clone https://github.com/R-dVL/automation-lab.git')
+                    Host host = new Host(HOST)
+                    print(host)
+            }
+        } catch(Exception err) {
+            println('ALERT | ${err.getMessage()}')
+            error('${err}')
         }
     }
 }
