93427569a9a91656da455802953f76dceccbb8b9
R-dVL <rauldel.valle.lima@gmail.com>
2023-08-09 14:23:20 +0200

    test

--- src/io/rdvl/automationLibrary/Host.groovy
+++ src/io/rdvl/automationLibrary/Host.groovy
@@ -47,14 +47,14 @@ class Host implements Serializable {
             // Retrieve info from Jenkins
             // User & Password
             pipeline.withCredentials([
-                pipeline.usernamePassword(credentialsId: "${configCredentials}", usernameVariable: 'user', passwordVariable: 'password')]) {
+                pipeline.usernamePassword(credentialsId: 'server-credentials', usernameVariable: 'user', passwordVariable: 'password')]) {
                     this.user = user
                     this.password = password
             }
 
             // IP
             pipeline.withCredentials([
-                pipeline.string(credentialsId: "${configIp}", variable: 'ip')]) {
+                pipeline.string(credentialsId: 'server-ip', variable: 'ip')]) {
                     this.ip = ip
             }
         }
