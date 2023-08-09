8937b8601615c19fd486cc58dc3f269e15278fc7
R-dVL <rauldel.valle.lima@gmail.com>
2023-08-09 11:37:17 +0200

    test

--- vars/test_pipeline.groovy
+++ vars/test_pipeline.groovy
@@ -11,8 +11,8 @@ def call() {
                     print(host)
             }
         } catch(Exception err) {
-            println('ALERT | ${err.getMessage()}')
-            error('${err}')
+            println("ALERT | ${err.getMessage()}")
+            error("${err}")
         }
     }
 }
