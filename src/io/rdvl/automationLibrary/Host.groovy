d6a77283317caae3f3d3f0d285b38596f3c6d392
R-dVL <rauldel.valle.lima@gmail.com>
2023-08-09 14:11:51 +0200

    test

--- src/io/rdvl/automationLibrary/Host.groovy
+++ src/io/rdvl/automationLibrary/Host.groovy
@@ -62,14 +62,13 @@ class Host implements Serializable {
     @NonCPS
     def sshCommand(cmd) {
         // Remote params
-        def remote = [
-            name = name
-            host = ip
-            user = user
-            password = password
-            port = 22
-            allowAnyHosts = true
-        ]
+        def remote = [:]
+        remote.name = name
+        remote.host = ip
+        remote.user = user
+        remote.password = password
+        remote.port = 22
+        remote.allowAnyHosts = true
 
         // Execute command
         pipeline.sshCommand remote: remote, command: cmd, sudo: false
