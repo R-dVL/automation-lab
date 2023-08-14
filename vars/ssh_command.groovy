0f4d5cad8885fe92c772d63c6523d2538a3fdbdf
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2023-08-14 22:53:37 +0200

    fix

--- vars/ssh_command.groovy
+++ vars/ssh_command.groovy
@@ -39,9 +39,6 @@ def call() {
                             host.setUser(user)
                             host.setPassword(password)
                     }
-                }
-
-                script {
                     // IP
                     withCredentials([
                         string(credentialsId: host.getConfigIp(), variable: 'ip')]) {
