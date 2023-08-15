c9419aa02f0f51bc54c3bdd525f57ed5c53b8c64
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2023-08-15 15:30:02 +0200

    test

--- vars/ssh_command.groovy
+++ vars/ssh_command.groovy
@@ -33,14 +33,17 @@ def call() {
             stage('Execute Command') {
                 host.sshCommand(CMD)
 
-                withCredentials([string(credentialsId: 'telegram-bot-token', variable: ‘TOKEN’),
-                string(credentialsId: 'telegram-chat-id', variable: ‘CHAT_ID’)]) {
-                sh """
-                curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${CHAT_ID} -d parse_mode=”HTML” -d text="<b>Project</b> : POC \
-                <b>Branch</b>: master \
-                <b>Build </b> : OK \
-                <b>Test suite</b> = Passed"
-                """
+                script {
+                    withCredentials([string(credentialsId: 'telegram-bot-token', variable: ‘TOKEN’),
+                    string(credentialsId: 'telegram-chat-id', variable: ‘CHAT_ID’)]) {
+                    sh """
+                    curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${CHAT_ID} -d parse_mode=”HTML” -d text="<b>Project</b> : POC \
+                    <b>Branch</b>: master \
+                    <b>Build </b> : OK \
+                    <b>Test suite</b> = Passed"
+                    """
+                    }
+                }
             }
 
         } catch(Exception err) {
