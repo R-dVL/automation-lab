24ebbce7ce820d1037b1c14782c3acdd2dc4a8f8
R-dVL <rauldel.valle.lima@gmail.com>
2023-08-20 18:32:58 +0200

    test

--- src/com/github/rdvl/automationLibrary/Project.groovy
+++ src/com/github/rdvl/automationLibrary/Project.groovy
@@ -24,7 +24,7 @@ public class Project {
             case 'maven':
                 pipeline.checkout(scm: [$class: 'GitSCM', userRemoteConfigs: [[url: url, credentialsId: 'github-token']], branches: [[name: version]]],poll: false)
 
-                def mvnHome = tool name: 'Maven 3.9.4', type: 'maven'
+                def mvnHome = pipeline.tool name: 'Maven 3.9.4', type: 'maven'
                 def mvnCmd = "${mvnHome}/bin/mvn"
 
                 pipeline.sh "${mvnCmd} clean package"
