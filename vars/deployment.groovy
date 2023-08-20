34923b4936b8c0bbfd257e415994c7515c4163a3
R-dVL <rauldel.valle.lima@gmail.com>
2023-08-20 13:26:37 +0200

    Test

--- vars/deployment.groovy
+++ vars/deployment.groovy
@@ -1,7 +1,11 @@
 package com.github.rdvl.automationLibrary
 
 def call() {
-    node {
+    agent {
+        docker {
+            image 'maven:3.9.3-eclipse-temurin-17-alpine'
+            args '-v /root/.m2:/root/.m2'
+        }
         // Environment variables
         environment {
             cfg
@@ -17,6 +21,7 @@ def call() {
                 Project prj = new Project(this, NAME, VERSION)
                 print(prj)
                 prj.downloadCode()
+                sh 'mvn -B -DskipTests clean package'
             }
 
         } catch(Exception err) {
