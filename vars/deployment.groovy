99446dc88d797c7422798820a6359774d8daec94
R-dVL <rauldel.valle.lima@gmail.com>
2023-08-21 12:23:24 +0200

    test

--- vars/deployment.groovy
+++ vars/deployment.groovy
@@ -36,7 +36,7 @@ def call() {
             }
 
             stage('Deploy') {
-                host.sshPut('/target/cat-watcher-v1.0.0.jar', '/home/rdvl/cat-watcher/artifacts/')
+                host.sshPut('target/cat-watcher-v1.0.0.jar', '/home/rdvl/cat-watcher/artifacts/')
             }
 
         } catch(Exception err) {
