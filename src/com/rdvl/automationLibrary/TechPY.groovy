20a8a0e16f3126d11d9fd237749e92e9cb032ebe
Raúl Del Valle <74815153+R-dVL@users.noreply.github.com>
2023-08-23 10:50:08 +0000

    test

--- src/com/rdvl/automationLibrary/TechPY.groovy
+++ src/com/rdvl/automationLibrary/TechPY.groovy
@@ -34,7 +34,7 @@ public class TechPY {
         try { pipeline.host.sshCommand("pm2 stop ${name}", true) } catch (Exception e) { pipeline.println('Already stopped..') }
 
         // Deploy
-        if(allreadyDeployed == 'false') {
+        if(this.allreadyDeployed == false) {
             pipeline.host.sshCommand("""mkdir /opt/apps/${name}/${version}
             cd /opt/apps/${name}/${version}
             git clone --depth 1 --branch ${version} ${url}
