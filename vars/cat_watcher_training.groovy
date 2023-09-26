1f9722d72e2eadf95d739b6cc7b4f412ae2a6046
R-dVL <rauldel.valle.lima@gmail.com>
2023-09-26 21:53:15 +0200

    test

--- vars/cat_watcher_training.groovy
+++ vars/cat_watcher_training.groovy
@@ -38,8 +38,7 @@ def call() {
             stage('Training') {
                 git branch: 'ai_model', url: 'https://github.com/R-dVL/cat-watcher.git'
                 sh("""
-                    cd ./cat-watcher
-                    cp //192.168.1.55/dataset ./resources
+                    cp //192.168.1.55/dataset ./
                     pip install -r requirements
                     python ./model/cat_identifyer.py
                 """)
