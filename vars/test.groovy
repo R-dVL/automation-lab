a96f306205ede944f75a6d81723fdf9717868be8
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2023-11-01 00:20:22 +0100

    test

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,29 +1,12 @@
 package com.rdvl.jenkinsLibrary
 
 def call() {
-    node {
-        // Environment variables
-        environment {
-            cfg
-            host
-        }
-        // Pipeline error control
+    node ('Docker') {
         try {
-            // Configuration instance
-            cfg = Configuration.getInstance()
-
-            // TODO: Retrieve host credentials in Host constructor
-            stage('Host Setup') {
-                git branch: 'master',
-                    url: 'https://github.com/R-dVL/ansible-playbooks.git'
+            stage('Test') {
+                echo 'TEST'
             }
-
-            stage('Execute Playbook') {
-                ansiblePlaybook playbook: "./playbooks/hello-world.yaml"
-            }
-
         } catch(Exception err) {
-            println("ALERT | Something went wrong")
             error(err.getMessage())
         }
     }
