27388ae35664ebe9daa54956b65eefb491488f7d
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2023-11-01 13:53:57 +0100

    test

--- vars/create_docker_image.groovy
+++ vars/create_docker_image.groovy
@@ -16,28 +16,25 @@ def call() {
                 script {
                     String dockerfile = libraryResource resource: 'Dockerfile'
                     writeFile file: 'Dockerfile', text: dockerfile
-
-                    String sshKey = libraryResource resource: 'keys/jenkins_agent_key.pub'
-                    writeFile file: 'jenkins_agent_key.pub', text: sshKey
                 }
             }
 
-            stage('Create and upload image') {
+            stage('Build and Push Image') {
                 String imageName = 'jenkins-agent'
                 String imageTag = 'latest'
                 withCredentials([
-                    string(credentialsId: 'github-package-token', variable: 'token')]) {
+                    string(credentialsId: 'dockerhub-token', variable: 'token')]) {
                         script {
-                            docker.withRegistry('https://docker.pkg.github.com', token) {
-                                def customImage = docker.build('R-dVL/jenkins-agent:latest', '.')
+                            docker.withRegistry('https://registry.hub.docker.com', token) {
+                                def customImage = docker.build('jenkins-agent:latest', '.')
                                 customImage.push()
                             }
                         }
+                    }
                 }
             }
 
         } catch(Exception err) {
-            println("ALERT | Something went wrong")
             error(err.getMessage())
         }
     }
