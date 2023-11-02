01bf6e92f3c232b44a29d63dc12622929c7f9223
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2023-11-02 22:22:01 +0100

    test

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -1,6 +1,7 @@
 package com.rdvl.jenkinsLibrary
 
 class utils {
+    @NonCPS
     def retrieveCredentials(credentialsId) {
         withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'user', passwordVariable: 'password')]) {
             return [user: user, password: password]
