8153eccabe860458c4e85c708d8fb6128d302c68
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2023-11-03 20:34:15 +0100

    test

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -1,10 +1,12 @@
 //// UTILS SINGLETON ////
 
-@NonCPS
 def retrieveCredentials(credentialsId) {
+    echo 'Funsiono?'
+    /*
     script {
         withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'user', passwordVariable: 'password')]) {
             return [user: user, password: password]
         }
     }
+    */
 }
