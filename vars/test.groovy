ab454b9bfd87367a79720d269d0467026b9c482b
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2023-11-05 00:24:36 +0100

    test

--- vars/test.groovy
+++ vars/test.groovy
@@ -4,11 +4,7 @@ def call() {
     node ('docker-agent') {
         try {
             sshagent(credentials: ['jenkins']) {
-            sh '''
-                [ -d ~/.ssh ] || mkdir ~/.ssh && chmod 0700 ~/.ssh
-                ssh-keyscan -t rsa,dsa 192.168.1.55 >> ~/.ssh/known_hosts
-                ssh jenkins@192.168.1.55 ...
-            '''
+            sh 'ls -altr'
             }
         } catch(Exception err) {
             error(err.getMessage())
