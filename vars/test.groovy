2fad8a922cda1e1e7afeb59c7201af6f926ecb19
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2023-11-05 00:22:38 +0100

    test

--- vars/test.groovy
+++ vars/test.groovy
@@ -3,8 +3,12 @@ package com.rdvl.jenkinsLibrary
 def call() {
     node ('docker-agent') {
         try {
-            def creds = utils.retrieveCredentials('server-credentials')
-            print creds
+            sshagent(credentials: ['jenkins']) {
+            sh '''
+                [ -d ~/.ssh ] || mkdir ~/.ssh && chmod 0700 ~/.ssh
+                ssh-keyscan -t rsa,dsa 192.168.1.55 >> ~/.ssh/known_hosts
+                ssh jenkins@192.168.1.55 ...
+            '''
         } catch(Exception err) {
             error(err.getMessage())
         }
