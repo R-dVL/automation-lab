583e53b2b8f864247c825fdfb23e9699d65570c3
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2023-11-05 14:46:26 +0100

    ansicolor

--- vars/ansible.groovy
+++ vars/ansible.groovy
@@ -2,27 +2,30 @@ package com.rdvl.jenkinsLibrary
 
 def call() {
     node ('docker-agent') {
-        try {
-            stage('Download Ansible Playbooks') {
-                git branch: 'master',
-                    url: 'https://github.com/R-dVL/ansible-playbooks.git'
-            }
-            // Configuration instance
-            String configurationJson = libraryResource resource: 'configuration.json'
-            configuration = readJSON text: configurationJson
+        ansiColor('xterm') {
+            try {
+                stage('Download Ansible Playbooks') {
+                    git branch: 'master',
+                        url: 'https://github.com/R-dVL/ansible-playbooks.git'
+                }
+                // Configuration instance
+                String configurationJson = libraryResource resource: 'configuration.json'
+                configuration = readJSON text: configurationJson
 
-            // Project to deploy
-            Project prj = new Project(this, 'lima-backend', '1.1.0')
+                // Project to deploy
+                Project prj = new Project(this, 'lima-backend', '1.1.0')
 
-            stage('Execute Playbook') {
-                ansiblePlaybook(
-                    inventory:'./inventories/hosts.yaml',
-                    playbook: "./playbooks/deploy-backend.yaml",
-                    credentialsId: 'jenkins',
-                    extras: "-e ${prj} -v")
+                stage('Execute Playbook') {
+                    ansiblePlaybook(
+                        inventory:'./inventories/hosts.yaml',
+                        playbook: "./playbooks/deploy-backend.yaml",
+                        credentialsId: 'jenkins',
+                        colorized: true,
+                        extras: "-e ${prj} -v")
+                }
+            } catch(Exception err) {
+                error(err.getMessage())
             }
-        } catch(Exception err) {
-            error(err.getMessage())
         }
     }
 }
