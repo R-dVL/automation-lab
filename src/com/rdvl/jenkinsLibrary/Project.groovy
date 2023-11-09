678698bcb60ef6cc43155b33f78bee0a0d7ad28d
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2023-11-09 22:26:44 +0100

    test

--- src/com/rdvl/jenkinsLibrary/Project.groovy
+++ src/com/rdvl/jenkinsLibrary/Project.groovy
@@ -41,6 +41,16 @@ public class Project {
 
     }
 
+    @NonCPS
+    def getName() {
+        return name
+    }
+
+    @NonCPS
+    def getVersion() {
+        return version
+    }
+
     // toString() method override to get a JSON of the class
     @Override
     @NonCPS
--- vars/deployment.groovy
+++ vars/deployment.groovy
@@ -62,8 +62,8 @@ def call() {
                 }
 
                 stage('Post Implantation') {
-                    host.sshGet("./', '/opt/apps/${project.getname()}/${project.getversion()}/${project.getname()}.log")
-                    archiveArtifacts artifacts: "./${project.getname()}.log"
+                    host.sshGet("./', '/opt/apps/${project.getName()}/${project.getVersion()}/${project.getName()}.log")
+                    archiveArtifacts artifacts: "./${project.getName()}.log"
                 }
 
             } catch(Exception e) {
