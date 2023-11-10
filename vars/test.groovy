9716080f5d81e03a5782cdf33b37577d676d803c
R-dVL <rauldel.valle.lima@gmail.com>
2023-11-10 15:14:50 +0100

    test

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,10 +1,39 @@
 package com.rdvl.jenkinsLibrary
 
 def call() {
-    node ('docker-agent') {
+    node ('server') {
+        environment {
+            user
+        }
         try {
-            sshagent(credentials: ['jenkins']) {
-            sh 'ls -altr'
+            user = 'R-dVL'
+
+            stage('Get Dockerfile') {
+                script {
+                    String dockerfile = libraryResource resource: 'Dockerfile'
+                    writeFile file: 'Dockerfile', text: dockerfile
+                }
+            }
+
+            stage('Build') {
+                script {
+                    sh ("docker build -t ghcr.io/${user}/jenkins-agent:latest .")
+                }
+            }
+
+            stage('Login') {
+                withCredentials([
+                    string(credentialsId: 'github-package-token	', variable: 'token')]) {
+                        script {
+                            sh "echo ${token} | docker login ghcr.io -u ${user} --password-stdin"
+                        }
+                    }
+            }
+
+            stage('Push') {
+                script {
+                    sh "docker push ghcr.io/${user}/jenkins-agent:latest"
+                }
             }
 
         } catch(Exception err) {
