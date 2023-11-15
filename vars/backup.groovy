240466227df9b43fbfced6e898a44f1d80f88a59
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2023-11-15 22:22:09 +0100

    test

--- vars/backup.groovy
+++ vars/backup.groovy
@@ -5,16 +5,42 @@ def call() {
         ansiColor('xterm') {
             environment {
                 configuration
-                project
                 host
             }
             try {
                 stage('Setup') {
                     cleanWs()
+                    // Configuration
+                    String configurationJson = libraryResource resource: 'configuration.json'
+                    configuration = readJSON text: configurationJson
+
+                    // Host init
+                    host = new Host(this, 'server')
+                    host.init()
 
                     // Donwload Ansible Playbooks
                     git branch: 'master',
                         url: 'https://github.com/R-dVL/ansible-playbooks.git'
+                }
+
+                stage('Connectivity Check') {
+                    // Host alive check
+                    // TODO: Dynamic host
+                    def pingResult = sh(script: "nc -z -w5 192.168.1.55 80", returnStatus: true)
+
+                    if (pingResult == 0) {
+                        println("Host reachable")
+                    } else {
+                        error("Host not reachable: ${pingResult}")
+                    }
+
+                    def sshResult = host.sshCommand('whoami')
+
+                    if (sshResult != 'jenkins') {
+                        error("SSH Connection failed: ${sshResult}")
+                    } else {
+                        println("Host accesible")
+                    }
                 }
 
                 stage('Backup') {
