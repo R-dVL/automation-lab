ef154803077ddfa18c1703a6231de28e9491801b
R-dVL <rauldel.valle.lima@gmail.com>
2023-11-15 17:21:56 +0100

    fix

--- vars/backup.groovy
+++ vars/backup.groovy
@@ -11,18 +11,6 @@ def call() {
                         url: 'https://github.com/R-dVL/ansible-playbooks.git'
                 }
 
-                stage('Connectivity Check') {
-                    // Host accesible with ssh key check
-                    try {
-                        sshagent(credentials: ['jenkins']) {
-                            sh(script: """ssh jenkins@192.168.1.55""")
-                            println("SSH connection OK")
-                        }
-                    } catch (e) {
-                        error("SSH connection error: ${e}")
-                    }
-                }
-
                 stage('Backup') {
                     ansiblePlaybook(
                         inventory:'./inventories/hosts.yaml',
