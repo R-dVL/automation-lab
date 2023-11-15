1ba6ae56d29b3165e4ce74cdea1954cce8bdbf7a
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2023-11-15 20:24:52 +0100

    test

--- vars/backup.groovy
+++ vars/backup.groovy
@@ -11,45 +11,17 @@ def call() {
             try {
                 stage('Setup') {
                     cleanWs()
-                    // Configuration
-                    String configurationJson = libraryResource resource: 'configuration.json'
-                    configuration = readJSON text: configurationJson
-
-                    host = new Host(this, 'server')
-                    host.init()
 
                     // Donwload Ansible Playbooks
                     git branch: 'master',
                         url: 'https://github.com/R-dVL/ansible-playbooks.git'
                 }
 
-                stage('Connectivity Check') {
-                    // Host alive check
-                    // TODO: Dynamic host
-                    def pingResult = sh(script: "nc -z -w5 192.168.1.55 80", returnStatus: true)
-
-                    if (pingResult == 0) {
-                        println("Host reachable")
-                    } else {
-                        error("Host not reachable: ${pingResult}")
-                    }
-
-                    // Host accesible with ssh key check
-                    try {
-                        sshagent(credentials: ['jenkins']) {
-                            sh(script: """ssh jenkins@192.168.1.55""")
-                            println("SSH connection OK")
-                        }
-                    } catch (e) {
-                        error("SSH connection error: ${e}")
-                    }
-                }
-
                 stage('Backup') {
                     ansiblePlaybook(
                         inventory:'./inventories/hosts.yaml',
                         playbook: "./playbooks/backup.yaml",
-                        credentialsId: 'jenkins',
+                        credentialsId: 'server-credentials',
                         colorized: true,
                         extras: "-e path=${PATH} -vvv")
                 }
