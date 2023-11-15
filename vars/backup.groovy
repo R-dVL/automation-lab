abc7c5b171b393b00c6f4b7b4bfc5fe78659dfe7
R-dVL <rauldel.valle.lima@gmail.com>
2023-11-15 16:45:26 +0100

    test

---
+++ vars/backup.groovy
@@ -0,0 +1,50 @@
+package com.rdvl.jenkinsLibrary
+
+def call() {
+    node ('docker-agent') {
+        ansiColor('xterm') {
+            try {
+                stage('Setup') {
+                    cleanWs()
+                    // Donwload Ansible Playbooks
+                    git branch: 'master',
+                        url: 'https://github.com/R-dVL/ansible-playbooks.git'
+                }
+
+                stage('Connectivity Check') {
+                    // Host alive check
+                    // TODO: Dynamic host
+                    def pingResult = sh(script: "nc -z -w5 192.168.1.55 80", returnStatus: true)
+
+                    if (pingResult == 0) {
+                        println("Host reachable")
+                    } else {
+                        error("Host not reachable: ${pingResult}")
+                    }
+
+                    // Host accesible with ssh key check
+                    try {
+                        sshagent(credentials: ['jenkins']) {
+                            sh(script: """ssh jenkins@192.168.1.55""")
+                            println("SSH connection OK")
+                        }
+                    } catch (e) {
+                        error("SSH connection error: ${e}")
+                    }
+                }
+
+                stage('Backup') {
+                    ansiblePlaybook(
+                        inventory:'./inventories/hosts.yaml',
+                        playbook: "./playbooks/backup.yaml",
+                        credentialsId: 'jenkins',
+                        colorized: true,
+                        extras: "-e ${PATH} -v")
+                }
+
+            } catch(Exception e) {
+                error(e.getMessage())
+            }
+        }
+    }
+}
