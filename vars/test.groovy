0da1a8b5ec0e27de577b94c77a2d68c6941f92ef
R-dVL <rauldel.valle.lima@gmail.com>
2023-11-15 15:42:51 +0100

    test

--- vars/test.groovy
+++ vars/test.groovy
@@ -3,32 +3,12 @@ package com.rdvl.jenkinsLibrary
 def call() {
     node ('server') {
         try {
-            stage('Get Dockerfile') {
-                script {
-                    String dockerfile = libraryResource resource: 'Dockerfile'
-                    writeFile file: 'Dockerfile', text: dockerfile
-                }
-            }
-
-            stage('Build') {
-                script {
-                    sh ("docker build -t ghcr.io/r-dvl/jenkins-library/jenkins-agent:latest .")
-                }
-            }
-
-            stage('Login') {
-                withCredentials([
-                    string(credentialsId: 'github-package-token	', variable: 'token')]) {
-                        script {
-                            sh "echo ${token} | docker login ghcr.io -u r-dvl --password-stdin"
-                        }
-                    }
-            }
-
-            stage('Push') {
-                script {
-                    sh "docker push ghcr.io/r-dvl/jenkins-library/jenkins-agent:latest"
-                }
+            stage('Test') {
+                ansiblePlaybook(
+                    inventory:'./inventories/hosts.yaml',
+                    playbook: "./playbooks/backup.yaml",
+                    credentialsId: 'jenkins',
+                    colorized: true)
             }
 
         } catch(Exception err) {
