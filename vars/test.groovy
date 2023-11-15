3d671442522b28ff641cd83d0c18ff62a5df350f
R-dVL <rauldel.valle.lima@gmail.com>
2023-11-15 15:44:16 +0100

    test

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,15 +1,38 @@
 package com.rdvl.jenkinsLibrary
 
 def call() {
-    node ('server') {
-        try {
-            stage('Test') {
-                ansiblePlaybook(
-                    inventory:'./inventories/hosts.yaml',
-                    playbook: "./playbooks/backup.yaml",
-                    credentialsId: 'jenkins',
-                    colorized: true)
+    node ('docker-agent') {
+        ansiColor('xterm') {
+            environment {
+                configuration
+                project
+                host
             }
+            try {
+                stage('Setup') {
+                    cleanWs()
+                    // Configuration
+                    String configurationJson = libraryResource resource: 'configuration.json'
+                    configuration = readJSON text: configurationJson
+
+                    // Project to deploy
+                    project = new Project(this, NAME, VERSION)
+                    project.init()
+
+                    host = new Host(this, 'server')
+                    host.init()
+
+                    // Donwload Ansible Playbooks
+                    git branch: 'master',
+                        url: 'https://github.com/R-dVL/ansible-playbooks.git'
+                }
+                stage('Test') {
+                    ansiblePlaybook(
+                        inventory:'./inventories/hosts.yaml',
+                        playbook: "./playbooks/backup.yaml",
+                        credentialsId: 'jenkins',
+                        colorized: true)
+                }
 
         } catch(Exception err) {
             error(err.getMessage())
