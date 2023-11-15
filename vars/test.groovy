fbe58dc52cb11762e0751577b2c97abd64851d18
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2023-11-15 22:23:23 +0100

    test

--- vars/test.groovy
+++ vars/test.groovy
@@ -3,24 +3,57 @@ package com.rdvl.jenkinsLibrary
 def call() {
     node ('docker-agent') {
         ansiColor('xterm') {
+            environment {
+                configuration
+                host
+            }
             try {
                 stage('Setup') {
                     cleanWs()
+                    // Configuration
+                    String configurationJson = libraryResource resource: 'configuration.json'
+                    configuration = readJSON text: configurationJson
+
+                    // Host init
+                    host = new Host(this, 'server')
+                    host.init()
+
                     // Donwload Ansible Playbooks
                     git branch: 'master',
                         url: 'https://github.com/R-dVL/ansible-playbooks.git'
                 }
 
-                stage('Test') {
+                stage('Connectivity Check') {
+                    // Host alive check
+                    // TODO: Dynamic host
+                    def pingResult = sh(script: "nc -z -w5 192.168.1.55 80", returnStatus: true)
+
+                    if (pingResult == 0) {
+                        println("Host reachable")
+                    } else {
+                        error("Host not reachable: ${pingResult}")
+                    }
+
+                    def sshResult = host.sshCommand('whoami')
+
+                    if (sshResult != 'jenkins') {
+                        error("SSH Connection failed: ${sshResult}")
+                    } else {
+                        println("Host accesible")
+                    }
+                }
+
+                stage('Backup') {
                     ansiblePlaybook(
                         inventory:'./inventories/hosts.yaml',
                         playbook: "./playbooks/backup.yaml",
-                        credentialsId: 'jenkins',
-                        colorized: true)
+                        credentialsId: 'server-credentials',
+                        colorized: true,
+                        extras: "-e {path: ${PATH}} -vvv")
                 }
 
-            } catch(Exception err) {
-                error(err.getMessage())
+            } catch(Exception e) {
+                error(e.getMessage())
             }
         }
     }
