01bcd277cba8664dee96ed967288831dfb6874ad
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2023-11-16 18:33:01 +0100

    test

--- vars/test.groovy
+++ vars/test.groovy
@@ -5,7 +5,18 @@ def call() {
         ansiColor('xterm') {
             try {
                 stage('TEST') {
-                    utils.log('ola en rojo', 'red')
+                    log = utils.log
+                    text = 'lorem ipsum dolor'
+                    log(text, 'black')
+                    log(text, 'red')
+                    log(text, 'green')
+                    log(text, 'yellow')
+                    log(text, 'blue')
+                    log(text, 'purple')
+                    log(text, 'cyan')
+                    log(text, 'white')
+                    log(text)
+                    log(text, 'inventao')
                 }
 
             } catch(Exception e) {
