04652e4ae37d7bef12250c0eb2b64c62bdf670bb
R-dVL <rauldel.valle.lima@gmail.com>
2023-11-16 11:35:24 +0100

    fix

--- vars/ssh_command.groovy
+++ vars/ssh_command.groovy
@@ -3,28 +3,50 @@ package com.rdvl.jenkinsLibrary
 
 def call() {
     node ('docker-agent') {
-        environment {
-            configuration
-            host
-        }
-        try {
-            stage('Setup') {
-                // Configuration instance
-                String configurationJson = libraryResource resource: 'configuration.json'
-                configuration = readJSON text: configurationJson
-
-                // Default Params
-                host = new Host(this, HOST)
-                host.init()
+        ansiColor('xterm') {
+            environment {
+                configuration
+                host
             }
+            try {
+                stage('Setup') {
+                    // Configuration instance
+                    String configurationJson = libraryResource resource: 'configuration.json'
+                    configuration = readJSON text: configurationJson
 
-            stage('Execute Command') {
-                def result = host.sshCommand(CMD, SUDO)
-                print result
+                    // Default Params
+                    host = new Host(this, HOST)
+                    host.init()
+                }
+
+                stage('Connectivity Check') {
+                    // Host alive check
+                    // TODO: Dynamic host
+                    def pingResult = sh(script: "nc -z -w5 192.168.1.55 80", returnStatus: true)
+
+                    if (pingResult == 0) {
+                        println("Host reachable")
+                    } else {
+                        error("Host not reachable: ${pingResult}")
+                    }
+
+                    def sshResult = host.sshCommand('whoami')
+
+                    if (sshResult != 'jenkins') {
+                        error("SSH Connection failed: ${sshResult}")
+                    } else {
+                        println("Host accesible")
+                    }
+                }
+
+                stage('Execute Command') {
+                    def result = host.sshCommand(CMD, SUDO)
+                    print("Result: ${result}")
+                }
+
+            } catch(Exception err) {
+                error(err.getMessage())
             }
-
-        } catch(Exception err) {
-            error(err.getMessage())
         }
     }
 }
