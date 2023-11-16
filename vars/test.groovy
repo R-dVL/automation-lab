85f533f909fbaeb099ab703fe71a13ab1495fa6e
R-dVL <rauldel.valle.lima@gmail.com>
2023-11-16 17:17:12 +0100

    Ansicolors logger

--- vars/test.groovy
+++ vars/test.groovy
@@ -3,61 +3,9 @@ package com.rdvl.jenkinsLibrary
 def call() {
     node ('docker-agent') {
         ansiColor('xterm') {
-            environment {
-                configuration
-                host
-                PATH
-            }
             try {
-                PATH='/DATA/Media'
-                params = """
-                    '{
-                        "path": "${PATH}"
-                    }'
-                """
-
-                stage('Setup') {
-                    cleanWs()
-                    // Configuration
-                    String configurationJson = libraryResource resource: 'configuration.json'
-                    configuration = readJSON text: configurationJson
-
-                    // Host init
-                    host = new Host(this, 'server')
-                    host.init()
-
-                    // Donwload Ansible Playbooks
-                    git branch: 'master',
-                        url: 'https://github.com/R-dVL/ansible-playbooks.git'
-                }
-
-                stage('Connectivity Check') {
-                    // Host alive check
-                    // TODO: Dynamic host
-                    def pingResult = sh(script: "nc -z -w5 192.168.1.55 80", returnStatus: true)
-
-                    if (pingResult == 0) {
-                        println("Host reachable")
-                    } else {
-                        error("Host not reachable: ${pingResult}")
-                    }
-
-                    def sshResult = host.sshCommand('whoami')
-
-                    if (sshResult != 'jenkins') {
-                        error("SSH Connection failed: ${sshResult}")
-                    } else {
-                        println("Host accesible")
-                    }
-                }
-
-                stage('Backup') {
-                    ansiblePlaybook(
-                        inventory:'./inventories/hosts.yaml',
-                        playbook: "./playbooks/backup.yaml",
-                        credentialsId: 'server-credentials',
-                        colorized: true,
-                        extras: "-e ${params} -vv")
+                stage('TEST') {
+                    utils.log('ola en rojo', 'red')
                 }
 
             } catch(Exception e) {
--- vars/utils.groovy
+++ vars/utils.groovy
@@ -6,3 +6,46 @@ def retrieveCredentials(credentialsId) {
             return [user: user, password: password]
     }
 }
+
+String log(text, color = 'none') {
+    switch(color) {
+        case 'black':
+            println("\e[30m${text}\e[0m")
+            break
+
+        case 'red':
+            println("\e[31m${text}\e[0m")
+            break
+
+        case 'green':
+            println("\e[32m${text}\e[0m")
+            break
+
+        case 'yellow':
+            println("\e[33m${text}\e[0m")
+            break
+        
+        case 'blue':
+            println("\e[34m${text}\e[0m")
+            break
+        
+        case 'purple':
+            println("\e[35m${text}\e[0m")
+            break
+        
+        case 'cyan':
+            println("\e[36m${text}\e[0m")
+            break
+
+        case 'white':
+            println("\e[37m${text}\e[0m")
+            break
+
+        case 'none':
+            println(text)
+            break
+
+        default:
+            println(text)
+    }
+}
