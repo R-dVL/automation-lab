f1d1f2fb6569a264b29553b811ded5a3fe71ae84
R-dVL <rauldel.valle.lima@gmail.com>
2023-11-23 09:10:25 +0100

    fix

--- vars/test.groovy
+++ vars/test.groovy
@@ -2,14 +2,12 @@ package com.rdvl.jenkinsLibrary
 
 def call() {
     node () {
-        ansiColor('xterm') {
-            try {
-                stage('Test') {
-                    echo 'test'
-                }
-            } catch(Exception err) {
-                error(err.getMessage())
+        try {
+            stage('Test') {
+                echo 'test'
             }
+        } catch(Exception err) {
+            error(err.getMessage())
         }
     }
 }
