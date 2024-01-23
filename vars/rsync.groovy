0e4d5dec1b2b6491e11e42df900eaba32ced5d84
Raúl Del Valle Lima <raul.del.valle.lima@kyndryl.com>
2024-01-23 12:47:38 +0100

    test

--- vars/rsync.groovy
+++ vars/rsync.groovy
@@ -27,17 +27,26 @@ def call() {
                     git branch: 'master',
                         url: 'https://github.com/r-dvl/ansible-playbooks.git'
                 }
-
+/*
                 stage('Connectivity Test') {
+                    // Host alive check
+                    def pingResult = sh(script: "nc -z -w5 ${host.getIp()} 80", returnStatus: true)
+
+                    if (pingResult == 0) {
+                        utils.log("Host reachable", 'green')
+                    } else {
+                        error("Host not reachable: ${pingResult}")
+                    }
+
                     // Host SSH accesible check
                     def sshResult = host.sshCommand('whoami')
-                    if (sshResult != 'rdvl') {
+                    if (sshResult != 'jenkins') {
                         error("SSH Connection failed: ${sshResult}")
                     } else {
                         utils.log("Host accesible", 'green')
                     }
                 }
-
+*/
                 stage('Rsync') {
                     ansiblePlaybook(
                         inventory:'./inventories/hosts.yaml',
