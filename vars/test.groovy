f269eca90f1e8152abe25e0ac30af91855971003
Raúl Del Valle Lima <raul.del.valle.lima@kyndryl.com>
2024-01-23 09:49:55 +0100

    fix

--- vars/test.groovy
+++ vars/test.groovy
@@ -2,23 +2,25 @@ package com.rdvl.jenkinsLibrary
 
 def call() {
     node () {
-        env {
-            image
-        }
-        try {
-            stage('Build image') {
-                git 'https://github.com/r-dvl/lima-backend.git'
-                image = docker.build('custom-jenkins:test')
+        ansiColor('xterm') {
+            environment {
+                image
             }
-
-            stage('Push image') {
-                def (user, password) = retrieveCredentials('github-user-password')
-                docker.withRegistry('https://ghcr.io', "docker login -u ${user} -p ${password}") {
-                    image.push()
+            try {
+                stage('Build image') {
+                    git 'https://github.com/r-dvl/lima-backend.git'
+                    image = docker.build('custom-jenkins:test')
                 }
+
+                stage('Push image') {
+                    def (user, password) = retrieveCredentials('github-user-password')
+                    docker.withRegistry('https://ghcr.io', "docker login -u ${user} -p ${password}") {
+                        image.push()
+                    }
+                }
+            } catch(Exception err) {
+                error(err.getMessage())
             }
-        } catch(Exception err) {
-            error(err.getMessage())
         }
     }
 }
