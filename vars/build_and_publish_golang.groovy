a2d4d5ecdb48ee628970a5a1ae01a1f9a05a96f7
Raúl Del Valle Lima <raul.del.valle.lima@kyndryl.com>
2024-01-24 09:59:34 +0100

    New Pipeline

---
+++ vars/build_and_publish_golang.groovy
@@ -0,0 +1,74 @@
+package com.rdvl.jenkinsLibrary
+
+def call() {
+    node () {
+        environment {
+            project
+            matrix
+            configuration
+        }
+        ansiColor('xterm') {
+            try {
+                stage('Setup') {
+                    cleanWs()
+                    // Read configuration file
+                    configuration = readJSON(text: libraryResource(resource: 'configuration.json'))
+
+                    // OS Binaries to build
+                    matrix = ['windows', 'linux', 'darwin']
+
+                    // Init project
+                    project = new Project(this, PROJECT_NAME, TAG)
+
+                    currentBuild.displayName = "${project.getName()} - ${project.getVersion()}"
+
+                    // Clone project repository
+                    // TODO: Use TAG env var to download selected version
+                    // git branch: 'master',
+                    //    url: 'https://github.com/R-dVL/ansible-playbooks.git'
+                    git "${project.getUrl()}"
+                }
+
+                stage('Build Binaries') {
+                    def parallelTech = [:]
+                    for (index in matrix) {
+                        def os = index
+                        parallelTech["${os}"] = {
+                            def builder = docker.build("ghcr.io/r-dvl/${project.getArtifactName()}:${TAG}", "--build-arg ${os}")
+                            builder.withRun("-v ./bin:/home/app/bin", "-e TAG=${TAG}")
+                        }
+                    }
+                }
+
+                stage('Upload Binaries') {
+                    def parallelTech = [:]
+                    for (index in matrix) {
+                        def os = index
+                        parallelTech["${os}"] = {
+                            switch(os) {
+                                case 'windows':
+                                    archiveArtifacts artifacts: "./bin/stay_active-${TAG}.${OS}-amd64.exe"
+                                    break
+
+                                case 'linux':
+                                    archiveArtifacts artifacts: "./bin/stay_active-${TAG}.${OS}-amd64.bin"
+                                    break
+
+                                case 'darwin':
+                                    archiveArtifacts artifacts: "./bin/stay_active-${TAG}.${OS}-amd64.app"
+                                    break
+
+                                default:
+                                    utils.log("Sistema Operativo no configurado", "red")
+                                    break
+                            }
+                        }
+                    }
+                }
+
+            } catch(Exception err) {
+                error(err.getMessage())
+            }
+        }
+    }
+}
